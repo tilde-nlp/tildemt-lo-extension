eba6c01d2a8837f0a0cff5dca4c92b6916aaa30d
Guntars Pužulis <guntars.puzulis@hotmail.com>
2020-10-21 14:19:37 +0300

    Refactor paragraph translation. Insert back original newlines

--- source/com/tilde/mt/lotranslator/helper/ContentHelper.java
+++ source/com/tilde/mt/lotranslator/helper/ContentHelper.java
@@ -1,6 +1,8 @@
 package com.tilde.mt.lotranslator.helper;
 
 
+import java.util.ArrayList;
+
 import com.sun.star.beans.UnknownPropertyException;
 import com.sun.star.beans.XPropertySet;
 import com.sun.star.lang.Locale;
@@ -53,15 +55,19 @@ public class ContentHelper {
 	 * all translated paragraphs are combined in a single variable.
 	 */
 	public static String combineTranslatedParagraphs(XComponentContext xContext, TildeMTClient apiClient, String systemID)  {
-		String result = "";
+		ArrayList<String> result = new ArrayList<String>();
 		
 		String selectedText = ContentHelper.getSelectedText(xContext).Text;
-
+		String newLine = "\n";
+		if(selectedText.indexOf("\r\n") > -1) {
+			newLine = "\r\n";
+		}
+		
 		if(selectedText.length() > 0) {
 			String paragraphs[] = selectedText.split("\\r?\\n");
-			int paralength = paragraphs.length;
-			// translate each paragraph separately
-			for(int i = 0; i != paralength; i++) {
+			int paragraphLength = paragraphs.length;
+			
+			for(int i = 0; i != paragraphLength; i++) {
 				String translation = "";
 				try {
 					translation = apiClient.Translate(systemID, paragraphs[i]).get();
@@ -69,38 +75,10 @@ public class ContentHelper {
 					e.printStackTrace();
 				}
 				
-				if(translation.length() > 0) {
-					result = combine(result, paralength, i, translation);
-				}
+				result.add(translation);
 			}
 		} 
 		
-		return result;
+		return String.join(newLine, result);
 	}
-
-	/**
-	 * Combine() changes combination logic based on how many paragraphs are translated.
-	 *   If only one paragraph is translated, put a single space before.
-	 *   If more than one paragraph is translated, start new paragraph
-	 *     and then split translated paragraphs too.
-	 *
-	 * @param paragraphCount			total paragraph count
-	 * @param currentParagraphNumber	current paragraph in the cycle
-	 * @param text						paragraph's content
-	 */
-	private static String combine(String result, int paragraphCount, int currentParagraphNumber, String text){
-		if (paragraphCount == 1) {
-			result = result.concat(" ");
-			result = result.concat(text);
-		}
-		if (paragraphCount > 1 && currentParagraphNumber == 0) {
-			result = result.concat("\n");
-		}
-		if (paragraphCount > 1) {
-			result = result.concat(text);
-			result = result.concat("\n");
-		}
-		return result;
-	}
-
 }
